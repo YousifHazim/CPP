80379e98a9f3cc264f997964b793525f6c205d7a
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-04-28 12:53:51 +0200

    Create B - Basic Data Types.cpp

---
+++ Problem Solving/Assiut Sheet/Sheet #1 (Data type - Conditions)/B - Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    long long num2;
+    char char1;
+    float num3;
+    double num4;
+
+    cin>>num1>>num2>>char1>>num3>>num4;
+    cout<<num1<<endl<<num2<<endl<<char1<<endl<<num3<<endl<<num4<<endl;
+
+    return 0;
+}

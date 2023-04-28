80ab7f4eb7cf66708b87a65ac66ded8a0d28ca70
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-04-28 13:42:19 +0200

    Create F - Digits Summation.cpp

---
+++ Problem Solving/Assiut Sheet/Sheet #1 (Data type - Conditions)/F - Digits Summation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long num1,num2;cin>>num1>>num2; // 13 12
+
+    int lastNum1 = num1 % 10;
+    int lastNum2= num2 % 10;
+
+    cout<<lastNum1 + lastNum2<<endl;
+
+
+    return 0;
+}

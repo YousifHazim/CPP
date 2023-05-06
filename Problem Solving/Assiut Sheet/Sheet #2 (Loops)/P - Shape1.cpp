2e147b0bec5ab7de1b13524842d7437bf9dfb0c9
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-05-06 21:51:11 +0300

    Create P - Shape1.cpp

---
+++ Problem Solving/Assiut Sheet/Sheet #2 (Loops)/P - Shape1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;cin>>number;
+
+    for(int i=number;i>=1;i--){
+
+        for(int j=i;j>0;j--){
+
+            cout<<'*';
+
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}

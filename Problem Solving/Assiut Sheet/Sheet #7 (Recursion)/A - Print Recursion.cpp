f95effc4aac4f8ab27d589175b6503f81f92042d
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-06-16 08:41:59 +0300

    Create A - Print Recursion.cpp

---
+++ Problem Solving/Assiut Sheet/Sheet #7 (Recursion)/A - Print Recursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int number);
+
+int main()
+{
+    int number;cin>>number;
+    print(number);
+
+    return 0;
+}
+
+void print(int number){
+
+    if(number == 0)
+        return;
+
+        cout<<"I love Recursion"<<endl;
+
+    return print(number-1);
+}
